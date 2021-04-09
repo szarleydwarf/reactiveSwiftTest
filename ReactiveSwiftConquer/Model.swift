9b12b49fc2d0b403eeae4b52f3b530585aa256e3
RJ <r.j.chomik@gmail.com>
2021-04-09 16:49:44 +0100

    first commit with basic layout and model

---
+++ ReactiveSwiftConquer/Model.swift
@@ -0,0 +1,19 @@
+//
+//  Model.swift
+//  ReactiveSwiftConquer
+//
+//  Created by The App Experts on 09/04/2021.
+//
+
+import Foundation
+//{
+//    "quoteText": "This is the final test of a gentleman: his respect for those who can be of no possible value to him. ",
+//    "quoteAuthor": "William Lyon Phelps",
+//    "senderName": "",
+//    "senderLink": "",
+//    "quoteLink": "http://forismatic.com/en/9d86f2a8a0/"
+//}
+struct Account {
+    let quoteText:String
+    let quoteAuthor :String
+}
---
+++ ReactiveSwiftConquer/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  ReactiveSwiftConquer
+//
+//  Created by The App Experts on 26/03/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var table: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
