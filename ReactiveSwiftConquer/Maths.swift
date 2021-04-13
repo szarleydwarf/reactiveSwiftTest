f3cb09bfcbbc579b04bc7c6030acebdadc2fff0b
RJ <r.j.chomik@gmail.com>
2021-04-13 17:35:41 +0100

    ignore

--- ReactiveSwiftConquer/Maths.swift
+++ ReactiveSwiftConquer/Maths.swift
@@ -11,7 +11,25 @@ class Maths {
     func calculate (from str: String) -> String {
         var toReturn:String = ""
         // example 6*(8/2)+2+10
-        
+        if str.count == 0 {
+            return "Nothing to calculate!"
+        }
+        let equasion = str.filter("0123456789+-/*()".contains)
+        var stack:[Int] = []
+        var num = 0
+        for c in equasion {
+            if c.isNumber {
+                num = num * 10 + Int(String(c))!
+            } else if c == "(" {
+                print("bracket")
+            } else {
+                print("SIGN")
+            }
+        }
+        print(" >> \(stack)")
+        // check if number
+        // check if symbol
+        // check if bracket open close
         
         return toReturn
     }
